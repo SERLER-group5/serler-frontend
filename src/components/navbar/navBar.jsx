017632d1075ca3cb0b797b373f3016e523df3eb6
Nimesh Trivedi <nimeshtrivedi@gmail.com>
2019-09-26 02:55:23 +1200

    fixed logo not showing bug

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        <img src="SerlerLogo.png" alt="" />
+        <img src="/SerlerLogo.png" alt="" />
       </Link>
       <button
         className="navbar-toggler"
